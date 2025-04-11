323718676fa2ad43ae18509a7b4df87e34739b32
Gian Ira B. Lariosa <gianlariosa@gmail.com>
2025-04-11 22:37:06 +0800

    Fixed react routing

--- v3-react/vite.config.js
+++ v3-react/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/v3-react/'
+  base: '/personal_website/v3-react/'
 })
