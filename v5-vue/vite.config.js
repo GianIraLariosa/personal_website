d9c726e111aa4a608365cb26145486f46919ebdd
Gian Ira B. Lariosa <gianlariosa@gmail.com>
2025-04-11 22:42:14 +0800

    Changed angular & vue routing

--- v5-vue/vite.config.js
+++ v5-vue/vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/v5-vue/',
+  base: '/personal_website/v5-vue/',
   plugins: [
     vue(),
     vueDevTools(),
